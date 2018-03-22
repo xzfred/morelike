3b8e4f139206741165fa278d523e05007f59977c
xzfred <xzfred@gmail.com>
2018-03-22 23:06:18 +0800

    init c7

--- src/file.rs
+++ src/file.rs
@@ -3,57 +3,93 @@ use std::collections::HashMap;
 use sha1::Sha1;
 use std::path::Path;
 use std::fs;
-use std::fs::FileType;
+use std::fs::Metadata;
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use crc32c_hw;
 
 #[derive(Debug)]
 pub struct FileInfo {
-    file_type: FileType,
     name: String,
     path: String,
     size: u64,
     sum: Sha1,
+    meta: Metadata,
 }
 
 impl FileInfo {
     pub fn new(path: &Path) -> FileInfo {
+        let meta = path.metadata().unwrap();
         FileInfo {
-            file_type: path.metadata().unwrap().file_type(),
             name: String::from(path.file_name().unwrap().to_str().unwrap()),
             path: String::from(path.to_str().unwrap()),
-            size: path.metadata().unwrap().len(),
+            size: meta.len(),
             sum: Sha1::default(),
+            meta: meta,
         }
     }
 }
 
+const BUFSIZE: usize = 1024;
+
 pub type LikeList = HashMap<String, FileInfo>;
 
-pub struct FileTable(HashMap<u32, LikeList>);
+pub struct FileTable {
+    // buf: [u8; 1024],
+    table: HashMap<u32, LikeList>,
+}
 
 impl FileTable {
     pub fn new() -> FileTable {
-        FileTable(HashMap::new())
+        FileTable {
+            table: HashMap::new(),
+            // buf: [0; 1024],
+        }
     }
 
-    pub fn scan(&self, path: &str) {
+    pub fn scan(&mut self, path: &str) {
         let path = Path::new(path);
-        self.load(path, 0);
+        self.load(&path, 0);
+        println!("{:?}", self.table);
     }
 
-    fn load(&self, parent: &Path, level: i32) {
-        let dirs = fs::read_dir(parent).unwrap();
-        let mut i: usize = 0;
+    fn load(&mut self, parent: &Path, level: i32) {
+        // let dirs = fs::read_dir(parent).unwrap();
 
-        let f_count = dirs.count();
+        // let f_count = dirs.count();
         let dirs = fs::read_dir(parent).unwrap();
 
         for f in dirs {
-            i += 1;
-            let ff = &f.unwrap().path(); // let buf = ff.to_str().unwrap().to_string();
-            println!("{:?}", FileInfo::new(ff));
+            let ff = &f.unwrap().path(); 
             if ff.is_dir() {
                 self.load(ff, level + 1);
+            } else {
+                let file_info = FileInfo::new(ff);
+                let sum = self.checksum(ff, &file_info);
+                self.table.entry(sum)
+                    .or_insert(HashMap::new())
+                    .insert(file_info.path.clone(), file_info);
             }
+        }
+    }
+
+    fn checksum(&self, file: &Path, file_info: &FileInfo) -> u32 {
+        let display = file.display();
+        let mut file: File = match File::open(&file) {
+            Err(why) => panic!("couldn't open {}: {}",
+                               display,
+                               why.description() ),
+            Ok(file) => file,
+        };
+        let mut buf: [u8; BUFSIZE] = [0; BUFSIZE];
+        if file_info.size > BUFSIZE as u64 {
+            file.read_exact(&mut buf).unwrap();
+            crc32c_hw::compute(buf.as_ref())
+        } else {
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer).unwrap();
+            crc32c_hw::compute(buffer.as_slice())
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ extern crate sha1;
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
+extern crate adler32;
+extern crate crc32c_hw;
+
+
 mod file;
 
 use file::{FileTable};
+// use std::convert::AsRef;
 
 fn main() {
     pretty_env_logger::init();
     info!("start");
-    let table = FileTable::new();
+    let mut table = FileTable::new();
     table.scan(".");
 }
