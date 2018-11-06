cdd9887ef7a84e9a8e81cbead8f2373115775b08
徐智 <xuzhi@mxtrip.cn>
2018-11-06 18:12:40 +0800

    尝试直接使用tokio

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,46 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
+#[allow(unused_imports)]
+
+use std::path::{Path, PathBuf};
+use std::io;
+use std::fs::{self, DirEntry};
 
 #[macro_use] extern crate log;
 extern crate pretty_env_logger;
-extern crate clap;
-extern crate indicatif;
-extern crate console;
-extern crate ignore;
-
-
-use clap::{Arg, App, ArgMatches};
-
-
-// extern crate adler32;
-// extern crate chrono;
-// extern crate console;
-// extern crate crc32c_hw;
-// extern crate sha1;
-// extern crate typenum;
-// extern crate digest;
-// #[macro_use] extern crate generic_array;
-// extern crate indicatif;
-// extern crate time;
-extern crate twox_hash;
-
-extern crate actix;
-extern crate futures;
+extern crate tokio_fs;
 extern crate tokio;
+extern crate futures;
 
-// pub mod task;
-// use task::{MsgPos, FinderMsg, Finder};
+use futures::{Future, Stream};
+// use tokio_fs::{ReadDir};
 use std::{thread, time};
-// use std::sync::{Arc};
 
-// use std::env;
-
-// mod finder;
-// mod taskpool;
-// mod sum;
-
-mod scan;
-mod sum;
 
 fn main() {
     pretty_env_logger::init();
     debug!("start: ++++++++++++++++++++++");
 
-    // finder::run();
-
-    // let ten_millis = time::Duration::from_millis(1);
-    // thread::sleep(ten_millis);
     debug!("end: ++++++++++++++++++++++");
+
+    load(".");
 }
 
-// finder::scan("/Users/xuzhi/my");
-
-// taskpool::ThreadPool::new().spawn(|| info!("i am thread!"));
-
-// let comparer = sum::Comparer::new();
-//     // f.scan("/Users/xuzhi/my/dev/morelike");
-// comparer.run("/Users/fred/my/dev/morelike/test");
-// comparer.run(&get_path("/my/zip"));
-// comparer.run(&get_matchs());
-
-// fn get_path(s: &str) -> String {
-//     let path = match env::var_os("HOME") {
-//         None => { println!("$HOME not set."); panic!(); }
-//         Some(path) => path.to_str().unwrap().to_owned() + s,
-//     };
-//     path
-// }
-
-fn get_matchs() -> String {
-    let matches = App::new("morelike")
-        .version("0.1")
-        .author("xzfred <xzfred@gmail.com>")
-        .about("what is morelike?")
-    // .arg(Arg::with_name("path")
-    //      .short("c")
-    //      .long("config")
-    //      .value_name("FILE")
-    //      .help("Sets a custom config file")
-    //      .takes_value(true))
-        .arg(Arg::with_name("INPUT")
-             .help("Sets the scan to path")
-             .required(true)
-             .index(1))
-    // .arg(Arg::with_name("v")
-    //      .short("v")
-    //      .multiple(true)
-    //      .help("Sets the level of verbosity"))
-        .get_matches();
-    matches.value_of("INPUT").unwrap().to_owned()
-
-    // if let Some(c) = matches.value_of("INPUT") {
-    //     println!("Value for -c: {:?}", c);
-    // }
+fn load<P>(path: P)
+where
+    P: AsRef<Path> + Send + 'static,
+{
+    let fut = tokio_fs::read_dir(path).flatten_stream().for_each(|dir| {
+        if dir.path().is_dir() {
+            println!("dir: {:?}", dir.path());
+            load(dir.path());
+        } else {
+            println!("file: {:?}", dir.path());
+        }
+        let ten_millis = time::Duration::from_millis(10);
+        thread::sleep(ten_millis);
+        Ok(())
+    }).map_err(|err| {
+        eprintln!("Error: {:?}", err);
+        ()
+    });
+    tokio::run(fut);
 }
---
+++ src/v3/main.rs
@@ -0,0 +1,98 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+#[macro_use] extern crate log;
+extern crate pretty_env_logger;
+extern crate clap;
+extern crate indicatif;
+extern crate console;
+extern crate ignore;
+
+
+use clap::{Arg, App, ArgMatches};
+
+// extern crate adler32;
+// extern crate chrono;
+// extern crate console;
+// extern crate crc32c_hw;
+// extern crate sha1;
+// extern crate typenum;
+// extern crate digest;
+// #[macro_use] extern crate generic_array;
+// extern crate indicatif;
+// extern crate time;
+extern crate twox_hash;
+
+extern crate actix;
+extern crate futures;
+extern crate tokio;
+
+// pub mod task;
+// use task::{MsgPos, FinderMsg, Finder};
+use std::{thread, time};
+// use std::sync::{Arc};
+
+// use std::env;
+
+// mod finder;
+// mod taskpool;
+// mod sum;
+
+mod scan;
+mod sum;
+
+fn main() {
+    pretty_env_logger::init();
+    debug!("start: ++++++++++++++++++++++");
+
+    // finder::run();
+
+    // let ten_millis = time::Duration::from_millis(1);
+    // thread::sleep(ten_millis);
+    debug!("end: ++++++++++++++++++++++");
+}
+
+// finder::scan("/Users/xuzhi/my");
+
+// taskpool::ThreadPool::new().spawn(|| info!("i am thread!"));
+
+// let comparer = sum::Comparer::new();
+//     // f.scan("/Users/xuzhi/my/dev/morelike");
+// comparer.run("/Users/fred/my/dev/morelike/test");
+// comparer.run(&get_path("/my/zip"));
+// comparer.run(&get_matchs());
+
+// fn get_path(s: &str) -> String {
+//     let path = match env::var_os("HOME") {
+//         None => { println!("$HOME not set."); panic!(); }
+//         Some(path) => path.to_str().unwrap().to_owned() + s,
+//     };
+//     path
+// }
+
+fn get_matchs() -> String {
+    let matches = App::new("morelike")
+        .version("0.1")
+        .author("xzfred <xzfred@gmail.com>")
+        .about("what is morelike?")
+    // .arg(Arg::with_name("path")
+    //      .short("c")
+    //      .long("config")
+    //      .value_name("FILE")
+    //      .help("Sets a custom config file")
+    //      .takes_value(true))
+        .arg(Arg::with_name("INPUT")
+             .help("Sets the scan to path")
+             .required(true)
+             .index(1))
+    // .arg(Arg::with_name("v")
+    //      .short("v")
+    //      .multiple(true)
+    //      .help("Sets the level of verbosity"))
+        .get_matches();
+    matches.value_of("INPUT").unwrap().to_owned()
+
+    // if let Some(c) = matches.value_of("INPUT") {
+    //     println!("Value for -c: {:?}", c);
+    // }
+}
